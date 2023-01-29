ca8d3462ca0e62bf6b69241daf8bf0f3e04e0dcc
gcminteligencia <gcmdepinteligencia@hotmail.com>
2023-01-29 10:01:51 -0300

    up

--- qr.py
+++ qr.py
@@ -3,13 +3,12 @@ import qrcode
 
 data = {
     '01': "https://intelligence-corporation.github.io/GCM-WWW/010001/mat00001.html",
-    '02': "https://intelligence-corporation.github.io/GCM-WWW/010001/mat00002.html",
-    '03': "https://intelligence-corporation.github.io/GCM-WWW/010001/mat00003.html",
+    '02': "https://intelligence-corporation.github.io/GCM-WWW/010001/mat00002.html"
 }
 
 if __name__ == '__main__':
-    colour = [(PG_BLUE, 'white'), ('white', PG_GREEN), (PG_BLUE, PG_GREEN)]
     for i, (key, value) in enumerate(data.items()):
-        qr = qrcode.QRCode(none, qrcode.ERROR_CORRECT_H, box_size=18, border=2, )
-        img = qr.make_image(fill_color=colour[i][0], back_color=)
-        img.save(f'{key}.py')
+        qr = qrcode.QRCode(qrcode.ERROR_CORRECT_H, box_size=18, border=2, )
+        qr.add_data(value)
+        img = qr.make_image(fill_color='black', back_color='white')
+        img.save(f'{key}.png')
