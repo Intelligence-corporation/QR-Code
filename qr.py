2167225e1fab505cffe85c32b00c785b2ebafe72
gcminteligencia <gcmdepinteligencia@hotmail.com>
2023-01-29 10:29:07 -0300

    up

--- qr.py
+++ qr.py
@@ -8,7 +8,7 @@ data = {
 
 if __name__ == '__main__':
     for i, (key, value) in enumerate(data.items()):
-        qr = qrcode.QRCode(qrcode.ERROR_CORRECT_H, box_size=18, border=2, )
+        qr = qrcode.QRCode(qrcode.ERROR_CORRECT_H, box_size=28, border=1)
         qr.add_data(value)
         img = qr.make_image(fill_color='black', back_color='white')
-        img.save(f'{key}.png')
+        img.save(f'qrcode/{key}.png')
