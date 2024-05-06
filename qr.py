2a5f63b769302d88e8ee06c759e81217e868e2e3
Angell Belger <angellbelger@hotmail.com>
2024-05-05 21:25:49 -0300

    new feature

--- qr.py
+++ qr.py
@@ -5,6 +5,16 @@ data = {
     'puzzle': "https://belgercompany.com/secrecy/miss_me.php",
 }
 
+print("Hello, world")
+
+ok = 1
+while (ok == 1):
+    quantify = int(input("How many QR-code do you want?"))
+    for c in range(0, quantify):
+        data[c] = str(input("link: "))
+    ok = 0
+
+
 if __name__ == '__main__':
     for i, (key, value) in enumerate(data.items()):
         qr = qrcode.QRCode(qrcode.ERROR_CORRECT_H, box_size=28, border=1)
