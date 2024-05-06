bd6b63655c1186f79ab9fb0099c8cfbada24fc9f
Angell Belger <angellbelger@hotmail.com>
2024-05-05 21:41:55 -0300

    update

--- qr.py
+++ qr.py
@@ -1,23 +1,33 @@
 import qrcode
 
-
-data = {
-    'puzzle': "https://belgercompany.com/secrecy/miss_me.php",
-}
+# Dicionário para armazenar os links
+links = {}
 
 print("Hello, world")
 
 ok = 1
-while (ok == 1):
-    quantify = int(input("How many QR-code do you want?"))
-    for c in range(0, quantify):
-        data[c] = str(input("link: "))
+while ok == 1:
+    quantify = int(input("How many QR codes do you want to generate? "))
+    for c in range(quantify):
+        link = input("Enter the link for QR code {}: ".format(c+1))
+        confirmation = input("Do you want to add this link? (s/n): ")
+        while confirmation.lower() not in ['s', 'n']:
+            confirmation = input("Please enter 's' for yes or 'n' for no: ")
+        if confirmation.lower() == 's':
+            links["QR{}".format(c+1)] = link
     ok = 0
 
-
-if __name__ == '__main__':
-    for i, (key, value) in enumerate(data.items()):
-        qr = qrcode.QRCode(qrcode.ERROR_CORRECT_H, box_size=28, border=1)
-        qr.add_data(value)
-        img = qr.make_image(fill_color='black', back_color='white')
-        img.save(f'qrcode/{key}.png')
+# Gerar QR codes para cada link
+for name, link in links.items():
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=28,
+        border=1,
+    )
+    qr.add_data(link)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("{}.png".format(name))
+    print("QR code generated for {}: {}.png".format(name, name))
+    print("Thank you for using my services. \nBy: Angell Belger")
